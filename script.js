c44ce596240862ed65bc515a79532cf74c07139d
Jan Lewandowski <iasiu@icloud.com>
2021-02-10 16:36:59 +0100

    Fixed alert messages, icecream type, price calculation

--- script.js
+++ script.js
@@ -13,21 +13,21 @@ function isValidName(name) {
 // This runs when the checkout button is clicked
 $('.checkout-button').click(function() {
   // Check whether the user wants a cone or a cup
-  const getIcecreamType = $('.icecream-type').val();
+  const getIcecreamType = ($('.icecream-type-1').is(':checked') == true) ? $('.icecream-type-1').val() : $('.icecream-type-2').val();
 
   // Get the user's name at checkout
   const getName = $('.name-for-checkout').val();
 
   // Check if the name input is empty
   if (!isValidName(getName)) {
-    alert('Please enter a name for the order!');
+    alert('Please enter a name for the order, otherwise someone else can take your icecream and you will be sad :(');
     return;
   }
 
   // This is the price of each product //
   const coconut_mango_price = 2.3;
   const salted_caramel_price = 2.5;
-  const strawberries_cream_price = 1.90;
+  const strawberries_cream_price = 1.9;
 
   // Get the quanity of scoops of items //
   const getQuantityCoconutMango = $('.quantity-scoops-coconut-mango').val();
@@ -35,9 +35,9 @@ $('.checkout-button').click(function() {
   const getQuantityStrawberriesCream = $('.quantity-scoops-strawberries-cream').val();
 
   // calculate the total cost of the order //
-  let total = coconut_mango_price * getQuantityCoconutMango;
+  let total = coconut_mango_price * getQuantityCoconutMango + salted_caramel_price * getQuantitySaltedCaramel + strawberries_cream_price * getQuantityStrawberriesCream;
 
-  let messageText = 'New order from: ' + getName + ' in type: ' + getIcecreamType + '. Your total order cost is: $' + total.toFixed(2);
+  let messageText = 'Hi ' + getName + ', we are happy to announce, that you\'ve just taken the first step to become happier by ordering your gelato in a ' + getIcecreamType + '. You only have to pay: $' + total.toFixed(2);
 
   // Alert with final amount
   alert(messageText);
